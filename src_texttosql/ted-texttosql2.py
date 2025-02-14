4d3d55bc2fc4d7385dc3c19265bd16dc1adba67e
ted-jung <jongnag@gmail.com>
2025-02-15 00:33:16 +0900

    two retrivers(obj, sql) and workflow

--- src_texttosql/ted-texttosql2.py
+++ src_texttosql/ted-texttosql2.py
@@ -1,12 +1,19 @@
+# ===========================================================================
+# ObjectIndex
+# Date: 6, Feb 2025
+# Writer: Ted, Jung
+# Description: ObjectIndex with (QueryEngine, Retriever)
+#              SQLTableRetrieverQueryEngine, 
+#              NLSQLRetriever, 
+#              RetrieverQueryEngine
+# ===========================================================================
+
+
 import matplotlib.pyplot as plt
 
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
-from llama_index.core import Settings
-
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-Settings.llm = Ollama(model="llama3.2", request_timeout=720.0)
-
+from llama_index.llms.openai import OpenAI
 
 from llama_index.core.indices.struct_store.sql_query import (
     SQLTableRetrieverQueryEngine,
@@ -16,6 +23,11 @@ from llama_index.core.objects import (
     ObjectIndex,
     SQLTableSchema,
 )
+
+from llama_index.core.retrievers import NLSQLRetriever
+from llama_index.core.response.notebook_utils import display_source_node
+from llama_index.core.query_engine import RetrieverQueryEngine
+
 from sqlalchemy import (
     create_engine,
     MetaData,
@@ -23,8 +35,17 @@ from sqlalchemy import (
     Column,
     String,
     Integer,
+    insert,
 )
-from llama_index.core import VectorStoreIndex, SQLDatabase
+
+from llama_index.core import VectorStoreIndex, SQLDatabase, Settings
+
+
+
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+Settings.llm = Ollama(model="llama3.2", request_timeout=720.0)
+# Settings.llm = OpenAI(model="gpt-4o-mini", request_timeout=720.0)
+
 
 engine = create_engine("sqlite:///:memory:")
 metadata_obj = MetaData()
@@ -36,26 +57,26 @@ city_stats_table = Table(
     Column("population", Integer),
     Column("country", String(16), nullable=False),
 )
+
 metadata_obj.create_all(engine)
 sql_database = SQLDatabase(engine, include_tables=["city_stats"])
 
-from sqlalchemy import insert
+
 
 rows = [
     {"city_name": "Toronto", "population": 2930000, "country": "Canada"},
     {"city_name": "Tokyo", "population": 13960000, "country": "Japan"},
-    {
-        "city_name": "Chicago",
-        "population": 2679000,
-        "country": "United States",
-    },
+    {"city_name": "Chicago", "population": 2679000, "country": "United States"},
     {"city_name": "Seoul", "population": 9776000, "country": "South Korea"},
 ]
+
+
 for row in rows:
     stmt = insert(city_stats_table).values(**row)
     with engine.begin() as connection:
         cursor = connection.execute(stmt)
-        
+
+
 # set Logging to DEBUG for more detailed outputs
 table_node_mapping = SQLTableNodeMapping(sql_database)
 table_schema_objs = [
@@ -72,8 +93,8 @@ obj_index = ObjectIndex.from_objects(
 query_engine = SQLTableRetrieverQueryEngine(
     sql_database, obj_index.as_retriever(similarity_top_k=1)
 )
-
 response = query_engine.query("Which city has the highest population?")
+
 #response.source_nodes
 for i in response.source_nodes:
     print(i.text)
@@ -82,27 +103,24 @@ response = query_engine.query("the sum of total population of all cities?")
 print(response)
 
 
-# Using Text-to-SQL Retriever
-from llama_index.core.retrievers import NLSQLRetriever
+# Using Text-to-SQL Retriever (NLSQLRetriever)
+
 
 # default retrieval (return_raw=True)
 nl_sql_retriever = NLSQLRetriever(
     sql_database, tables=["city_stats"], return_raw=True
 )
-
 results = nl_sql_retriever.retrieve(
     "Return the top 5 cities (along with their populations) with the highest population."
 )
+
 for result in results:
     print(result)
 
-from llama_index.core.response.notebook_utils import display_source_node
 
 for n in results:
     display_source_node(n)
 
-
-from llama_index.core.query_engine import RetrieverQueryEngine
 
 query_engine = RetrieverQueryEngine.from_args(nl_sql_retriever)
 response = query_engine.query(
