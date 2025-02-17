e1852e71d37e641d9278d9736a5dfb571472c06d
ted-jung <jongnag@gmail.com>
2025-02-17 22:08:13 +0900

    persist index to remote store

--- src_storage/ted-persist-remotestorage.py
+++ src_storage/ted-persist-remotestorage.py
@@ -1,7 +1,7 @@
 # ===========================================================================
 # Persist index to Remote Store
 # Created: 6, Feb 2025
-# Updated: 6, Feb 2025
+# Updated: 17, Feb 2025
 # Writer: Ted, Jung
 # Description: 
 #   1. prepare data
@@ -11,17 +11,25 @@
 # ===========================================================================
 
 
+import textwrap
 import clickhouse_connect
 
+from urllib import response
+from warnings import filters
 
 
 from llama_index.core import VectorStoreIndex, StorageContext
-from llama_index.vector_stores.clickhouse import ClickHouseVectorStore
 from llama_index.core.schema import TextNode, Document
 
+from llama_index.vector_stores.clickhouse import ClickHouseVectorStore
+from llama_index.core.vector_stores import ExactMatchFilter, MetadataFilters
+
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.openai import OpenAI
 
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+llm = OpenAI(model="gpt-4o-mini", temperature=0.2)
+
 
 def textnode_to_document(textnode):
   """
@@ -105,6 +113,8 @@ nodes = [
 ]
 
 
+
+# Create a client to connect ClickHouse
 ch_client = clickhouse_connect.get_client(
     host="localhost",
     port=8123,
@@ -113,13 +123,40 @@ ch_client = clickhouse_connect.get_client(
     database="default",
 )
 
+
+
+# Create VectorStore on Store(ClickHouse)
+# A utility container for storing nodes,indices, and vectors.
+# Turn nodes to documents
+# Create an index based on documents and persist it
+
+documents = [textnode_to_document(node) for node in nodes]
+
 vector_store = ClickHouseVectorStore(
     ch_client, 
     table="quickstart_index",
     embed_model=embed_model
 )
-
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
-documents = [textnode_to_document(node) for node in nodes]
 index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+
 storage_context.persist()
+
+
+query_engine = index.as_query_engine(
+    filters=MetadataFilters(filters=[ExactMatchFilter(key="category", value="Entertainment")]),
+    similarity_top_k=2,
+    vector_store_query_mode="hybrid",
+    llm=llm
+)
+
+
+response = query_engine.query("Who is Angelina Jolie? add regarding shes family")
+print(textwrap.fill(str(response), 100))
+
+
+
+# Clear All indexes -> vectorstore -> clickhouse
+
+for document in documents:
+  index.delete_ref_doc(document.doc_id)
